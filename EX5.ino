5ff69cabd8ddb5287c3cda5a319c037e9260e823
unknown <gusspansa@student.gu.se>
2019-02-23 18:57:43 +0100

    correctly displaying the desired 0-9 digits for both segment display parts

--- EX5.ino
+++ EX5.ino
@@ -5,51 +5,13 @@ int convertTemperature(int temp){
   return 1;
 }
 
-void setup() {
-  // put your setup code here, to run once:
-#include <util/delay.h>
-
-#define REG8(x) *((unsigned char *) (x))     // A macro to use for IO R/W
-#define OUT_PORTD   0x2B
-#define IN_PORTB    0x23
-#define DDR_D       0x2A
-#define DDR_B       0x24
-#define BLINK_DELAY_MS  1000
-#define DDRC 0xff
-
-
-
-  
-
-Serial.begin(9600);
-
-pinMode(2,OUTPUT);
-pinMode(3,OUTPUT);
-pinMode(4,OUTPUT);
-pinMode(5,OUTPUT);
-pinMode(6,OUTPUT);
-pinMode(7,OUTPUT);
-pinMode(8,OUTPUT);
-pinMode(10,OUTPUT);
-pinMode(11,OUTPUT);
-
-digitalWrite(2,HIGH);
-digitalWrite(3,LOW);
-digitalWrite(4,LOW);
-digitalWrite(5,LOW);
-digitalWrite(6,LOW);
-digitalWrite(7,LOW);
-digitalWrite(8,HIGH);
-
-digitalWrite(10,LOW); //PIN 5 CATHODE
-digitalWrite(11,LOW); //PIN 10 CATHODE
-}
 
 void display(int d) {
   if (d >= 0 || d <= 9) {
-
+    Serial.println("ciao");
     switch(d) {
       case 1:
+      Serial.println("case 1");
         digitalWrite(2, HIGH);
         digitalWrite(3, LOW);
         digitalWrite(4, LOW);
@@ -59,6 +21,7 @@ void display(int d) {
         digitalWrite(8, HIGH);
         break;
       case 2:
+            Serial.println("case 2");
         digitalWrite(2, LOW);
         digitalWrite(3, HIGH);
         digitalWrite(4, LOW);
@@ -68,6 +31,8 @@ void display(int d) {
         digitalWrite(8, HIGH);
         break;
       case 3:
+            Serial.println("case 3");
+
         digitalWrite(2, HIGH);
         digitalWrite(3, HIGH);
         digitalWrite(4, LOW);
@@ -77,6 +42,8 @@ void display(int d) {
         digitalWrite(8, HIGH);
         break;
       case 4:
+            Serial.println("case 4");
+
         digitalWrite(2, HIGH);
         digitalWrite(3, HIGH);
         digitalWrite(4, HIGH);
@@ -86,6 +53,8 @@ void display(int d) {
         digitalWrite(8, HIGH);
         break;
       case 5:
+            Serial.println("case 5");
+
         digitalWrite(2, LOW);
         digitalWrite(3, HIGH);
         digitalWrite(4, LOW);
@@ -121,7 +90,19 @@ void display(int d) {
         digitalWrite(7, HIGH);
         digitalWrite(8, HIGH);
         break;
+
+      case 9:
+        digitalWrite(2, HIGH);
+        digitalWrite(3, HIGH);
+        digitalWrite(4, HIGH);
+        digitalWrite(5, HIGH);
+        digitalWrite(6, LOW);
+        digitalWrite(7, LOW);
+        digitalWrite(8, HIGH);
+        break;
+        
       default:
+      Serial.println("DEFAULT");
         digitalWrite(2, HIGH);
         digitalWrite(3, LOW);
         digitalWrite(4, HIGH);
@@ -135,16 +116,50 @@ void display(int d) {
   }
 }
 
+
+void setup() {
+  // put your setup code here, to run once:
+#include <util/delay.h>
+
+#define REG8(x) *((unsigned char *) (x))     // A macro to use for IO R/W
+#define OUT_PORTD   0x2B
+#define IN_PORTB    0x23
+#define DDR_D       0x2A
+#define DDR_B       0x24
+#define BLINK_DELAY_MS  1000
+#define DDRC 0xff
+
+
+
+  
+
+Serial.begin(9600);
+
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(7,OUTPUT);
+pinMode(8,OUTPUT);
+
+pinMode(10,OUTPUT);
+pinMode(11,OUTPUT);
+
+digitalWrite(10,LOW); //PIN 5 CATHODE
+digitalWrite(11,LOW); //PIN 10 CATHODE
+}
+
+
 void loop() {
-int temperature = convertTemperature (analogRead(analogPin));
-Serial.println(temperature);
+
+display(1);
+digitalWrite(11,LOW); //set one digit to turned on
+digitalWrite(10,HIGH); //the other to turned off
 
 delay(10);
-
-digitalWrite(10,LOW);
 digitalWrite(11,HIGH);
+digitalWrite(10,LOW);
+display(3);
 delay(10);
-digitalWrite(10,HIGH);
-digitalWrite(11,LOW);
-
 }
