4651c801ea1be79337ddff0b8068f6650907c330
Trooper98 <oliver.g.ryan@gmail.com>
2019-02-28 21:59:59 +0100

    added notes and complied to documentation

--- EX5.ino
+++ EX5.ino
@@ -1,13 +1,18 @@
+//temperature sensor
 long temperature = 0;
-int tempPin = 0;
-const int switch1 = 10;
-const int switch2 = 11;
+int tempPin = A0;
 int rightTemp = 0;
 int leftTemp = 0;
 int maxRight = 0;
 int maxLeft = 0;
 int currentTemp = 0;
 int maxTemp = 0;
+
+//dip switch
+const int switch1 = 10;
+const int switch2 = 11;
+
+//7 segment display
 int a = 0;
 int b = 1;
 int c = 2;
@@ -17,6 +22,7 @@ int f = 5;
 int g = 6;
 int cathode1 = 8;
 int cathode2 = 9;
+
 void setup()
 {
   // pinMode(tempPin, INPUT); //temp
@@ -33,81 +39,40 @@ void setup()
   digitalWrite(cathode1, LOW); //PIN 5 CATHODE
   digitalWrite(cathode2, LOW); //PIN 9 CATHODE
 
-
   pinMode(switch1, INPUT);
-    pinMode(switch2, INPUT);
-
+  pinMode(switch2, INPUT);
 
   // put your setup code here, to run once:
 }
 
-int getTemp(){
-  currentTemp = analogRead(A0);
-  currentTemp = (5.0 * currentTemp * 100.0)/1024.0;
-  Serial.print(currentTemp); Serial.println(" degrees C"); 
+int getTemp()
+{
+  currentTemp = analogRead(tempPin);
+  currentTemp = (5.0 * currentTemp * 100.0) / 1024.0;
+  Serial.print(currentTemp);
+  Serial.println(" degrees C");
   return currentTemp;
 }
 
-void showLeft(int left){
+void showLeft(int left)
+{
   digitalWrite(8, HIGH);
   digitalWrite(9, LOW);
   display(left);
 }
 
-void showRight(int right){
+void showRight(int right)
+{
   digitalWrite(8, LOW);
   digitalWrite(9, HIGH);
   display(right);
-
 }
-
-void loop(){
-   int state1 = digitalRead(switch1);
-   int state2 = digitalRead(switch2);
-   Serial.print(state1);    Serial.println(" state 1");
-   Serial.print(state2);    Serial.println(" state 2");
-
-   
-  currentTemp = getTemp();
-    if(currentTemp > maxTemp){
-      maxTemp = currentTemp;
-    }
-    rightTemp = currentTemp % 10;
-    leftTemp = currentTemp / 10;
-
-    Serial.println("CurrentTemp");
-    Serial.println(currentTemp);
-  unsigned long startTime = millis();
-    Serial.println("normal"); 
-    
-  for (unsigned long elapsed = 0; elapsed < 600; elapsed = millis() - startTime ){
-      if(state1 == LOW && state2 == LOW){
-
-    showLeft(leftTemp);
-    delay(5);
-    showRight(rightTemp);
-    delay(5);
-      }
-      else{
-    int maxLeft = maxTemp / 10;
-    int maxRight = maxTemp % 10;
-showLeft(maxLeft);
-    delay(5);
-    showRight(maxRight);
-    delay(5);  }
-    } 
- 
-  }
-  
-
-
-
 
 void display(int d)
 {
   if (d >= 0 || d <= 9)
   {
-    
+
     switch (d)
     {
     case 1:
@@ -211,6 +176,50 @@ void display(int d)
       digitalWrite(5, LOW);
       digitalWrite(6, HIGH);
       break;
+    }
+  }
+}
+
+void loop()
+{
+  int state1 = digitalRead(switch1);
+  int state2 = digitalRead(switch2);
+  Serial.print(state1);
+  Serial.println(" state 1");
+  Serial.print(state2);
+  Serial.println(" state 2");
+
+  currentTemp = getTemp();
+  if (currentTemp > maxTemp)
+  {
+    maxTemp = currentTemp;
+  }
+  rightTemp = currentTemp % 10;
+  leftTemp = currentTemp / 10;
+
+  Serial.println("CurrentTemp");
+  Serial.println(currentTemp);
+  unsigned long startTime = millis();
+  Serial.println("normal");
+
+  for (unsigned long elapsed = 0; elapsed < 600; elapsed = millis() - startTime)
+  {
+    if (state1 == LOW && state2 == LOW)
+    {
+
+      showLeft(leftTemp);
+      delay(5);
+      showRight(rightTemp);
+      delay(5);
+    }
+    else if (state1 == HIGH && state2 == HIGH)
+    {
+      int maxLeft = maxTemp / 10;
+      int maxRight = maxTemp % 10;
+      showLeft(maxLeft);
+      delay(5);
+      showRight(maxRight);
+      delay(5);
     }
   }
 }
