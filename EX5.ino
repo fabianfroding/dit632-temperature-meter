8a6bce2646ca9da8ad415a560c51217b8526ff7e
duykamke <gusngudu@student.gu.se>
2019-02-28 20:58:50 +0100

    final

--- EX5.ino
+++ EX5.ino
@@ -1,201 +1,216 @@
 long temperature = 0;
-int analogPin = A0;
-int dipSwitchPin1 = 10;
-int dipSwitchPin2 = 11;
-
+int tempPin = 0;
+const int switch1 = 10;
+const int switch2 = 11;
+int rightTemp = 0;
+int leftTemp = 0;
+int maxRight = 0;
+int maxLeft = 0;
+int currentTemp = 0;
+int maxTemp = 0;
+int a = 0;
+int b = 1;
+int c = 2;
+int d = 3;
+int e = 4;
+int f = 5;
+int g = 6;
+int cathode1 = 8;
+int cathode2 = 9;
 void setup()
 {
+  // pinMode(tempPin, INPUT); //temp
 
-  Serial.begin(9600);
+  pinMode(a, OUTPUT);          //7 digit display
+  pinMode(b, OUTPUT);          //7 digit display
+  pinMode(c, OUTPUT);          //7 digit display
+  pinMode(d, OUTPUT);          //7 digit display
+  pinMode(e, OUTPUT);          //7 digit display
+  pinMode(f, OUTPUT);          //7 digit display
+  pinMode(g, OUTPUT);          //7 digit display
+  pinMode(cathode1, OUTPUT);   //7 digit display
+  pinMode(cathode2, OUTPUT);   //7 digit display
+  digitalWrite(cathode1, LOW); //PIN 5 CATHODE
+  digitalWrite(cathode2, LOW); //PIN 9 CATHODE
 
-  pinMode(dipSwitchPin1, INPUT); //dip switch
-  pinMode(dipSwitchPin2, INPUT); //dip switch
 
-  pinMode(2, OUTPUT); //7 digit display
-  pinMode(3, OUTPUT); //7 digit display
-  pinMode(4, OUTPUT); //7 digit display
-  pinMode(5, OUTPUT); //7 digit display
-  pinMode(6, OUTPUT); //7 digit display
-  pinMode(7, OUTPUT); //7 digit display
-  pinMode(8, OUTPUT); //7 digit display
+  pinMode(switch1, INPUT);
+    pinMode(switch2, INPUT);
 
-  pinMode(9, OUTPUT);  //7 digit display
-  pinMode(12, OUTPUT); //7 digit display
 
-  digitalWrite(9, LOW);  //PIN 5 CATHODE
-  digitalWrite(12, LOW); //PIN 9 CATHODE
+  // put your setup code here, to run once:
 }
 
-int convertTemperature(int temp)
-{
-  //temp algorithm
-  return 1;
+int getTemp(){
+  currentTemp = analogRead(A0);
+  currentTemp = (5.0 * currentTemp * 100.0)/1024.0;
+  Serial.print(currentTemp); Serial.println(" degrees C"); 
+  return currentTemp;
 }
+
+void showLeft(int left){
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+  display(left);
+}
+
+void showRight(int right){
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  display(right);
+
+}
+
+void loop(){
+   int state1 = digitalRead(switch1);
+   int state2 = digitalRead(switch2);
+   Serial.print(state1);    Serial.println(" state 1");
+   Serial.print(state2);    Serial.println(" state 2");
+
+   
+  currentTemp = getTemp();
+    if(currentTemp > maxTemp){
+      maxTemp = currentTemp;
+    }
+    rightTemp = currentTemp % 10;
+    leftTemp = currentTemp / 10;
+
+    Serial.println("CurrentTemp");
+    Serial.println(currentTemp);
+  unsigned long startTime = millis();
+    Serial.println("normal"); 
+    
+  for (unsigned long elapsed = 0; elapsed < 600; elapsed = millis() - startTime ){
+      if(state1 == LOW && state2 == LOW){
+
+    showLeft(leftTemp);
+    delay(5);
+    showRight(rightTemp);
+    delay(5);
+      }
+      else{
+    int maxLeft = maxTemp / 10;
+    int maxRight = maxTemp % 10;
+showLeft(maxLeft);
+    delay(5);
+    showRight(maxRight);
+    delay(5);  }
+    } 
+ 
+  }
+  
+
+
+
 
 void display(int d)
 {
   if (d >= 0 || d <= 9)
   {
-    Serial.println("ciao");
+    
     switch (d)
     {
     case 1:
       Serial.println("case 1");
+      digitalWrite(0, LOW);
+      digitalWrite(1, HIGH);
       digitalWrite(2, HIGH);
       digitalWrite(3, LOW);
       digitalWrite(4, LOW);
       digitalWrite(5, LOW);
       digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
-      digitalWrite(8, HIGH);
       break;
     case 2:
       Serial.println("case 2");
+      digitalWrite(0, HIGH);
+      digitalWrite(1, HIGH);
       digitalWrite(2, LOW);
       digitalWrite(3, HIGH);
-      digitalWrite(4, LOW);
-      digitalWrite(5, HIGH);
+      digitalWrite(4, HIGH);
+      digitalWrite(5, LOW);
       digitalWrite(6, HIGH);
-      digitalWrite(7, HIGH);
-      digitalWrite(8, HIGH);
       break;
     case 3:
       Serial.println("case 3");
 
+      digitalWrite(0, HIGH);
+      digitalWrite(1, HIGH);
       digitalWrite(2, HIGH);
       digitalWrite(3, HIGH);
       digitalWrite(4, LOW);
-      digitalWrite(5, HIGH);
-      digitalWrite(6, LOW);
-      digitalWrite(7, HIGH);
-      digitalWrite(8, HIGH);
+      digitalWrite(5, LOW);
+      digitalWrite(6, HIGH);
       break;
     case 4:
       Serial.println("case 4");
 
+      digitalWrite(0, LOW);
+      digitalWrite(1, HIGH);
       digitalWrite(2, HIGH);
-      digitalWrite(3, HIGH);
-      digitalWrite(4, HIGH);
-      digitalWrite(5, LOW);
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
-      digitalWrite(8, HIGH);
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, HIGH);
+      digitalWrite(6, HIGH);
       break;
     case 5:
       Serial.println("case 5");
 
-      digitalWrite(2, HIGH);
-      digitalWrite(3, HIGH);
-      digitalWrite(4, HIGH);
-      digitalWrite(5, HIGH);
-      digitalWrite(6, LOW);
-      digitalWrite(7, HIGH);
-      digitalWrite(8, LOW);
-      break;
-    case 6:
+      digitalWrite(0, HIGH);
+      digitalWrite(1, LOW);
       digitalWrite(2, HIGH);
       digitalWrite(3, HIGH);
       digitalWrite(4, LOW);
       digitalWrite(5, HIGH);
       digitalWrite(6, HIGH);
-      digitalWrite(7, HIGH);
-      digitalWrite(8, HIGH);
+      break;
+    case 6:
+      digitalWrite(0, HIGH);
+      digitalWrite(1, LOW);
+      digitalWrite(2, HIGH);
+      digitalWrite(3, HIGH);
+      digitalWrite(4, HIGH);
+      digitalWrite(5, HIGH);
+      digitalWrite(6, HIGH);
       break;
     case 7:
+      digitalWrite(0, HIGH);
+      digitalWrite(1, HIGH);
       digitalWrite(2, HIGH);
       digitalWrite(3, LOW);
       digitalWrite(4, LOW);
-      digitalWrite(5, HIGH);
+      digitalWrite(5, LOW);
       digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
-      digitalWrite(8, HIGH);
       break;
     case 8:
+      digitalWrite(0, HIGH);
+      digitalWrite(1, HIGH);
       digitalWrite(2, HIGH);
       digitalWrite(3, HIGH);
       digitalWrite(4, HIGH);
       digitalWrite(5, HIGH);
       digitalWrite(6, HIGH);
-      digitalWrite(7, HIGH);
-      digitalWrite(8, HIGH);
       break;
 
     case 9:
+      digitalWrite(0, HIGH);
+      digitalWrite(1, HIGH);
       digitalWrite(2, HIGH);
       digitalWrite(3, HIGH);
-      digitalWrite(4, HIGH);
+      digitalWrite(4, LOW);
       digitalWrite(5, HIGH);
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
-      digitalWrite(8, HIGH);
+      digitalWrite(6, HIGH);
       break;
 
     default:
       Serial.println("DEFAULT");
-      digitalWrite(2, HIGH);
+      digitalWrite(0, HIGH);
+      digitalWrite(1, LOW);
+      digitalWrite(2, LOW);
       digitalWrite(3, LOW);
-      digitalWrite(4, HIGH);
-      digitalWrite(5, HIGH);
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
       digitalWrite(6, HIGH);
-      digitalWrite(7, HIGH);
-      digitalWrite(8, HIGH);
       break;
     }
   }
-}
-
-void loop()
-{
-
-  if (digitalRead(dipSwitchPin1) == 1)
-  {
-    Serial.println("HIGH");
-    Serial.println(digitalRead(dipSwitchPin1));
-    Serial.println(digitalRead(dipSwitchPin2));
-    //show max temperature [placeholder=  1 && 1]
-
-    display(0);
-    digitalWrite(12, LOW);
-    digitalWrite(9, HIGH);
-
-    delay(10);
-    digitalWrite(12, HIGH);
-    digitalWrite(9, LOW);
-    display(0);
-    delay(10);
-  }
-  else if (digitalRead(dipSwitchPin1) == 0)
-  {
-    Serial.println("LOW");
-    Serial.println(digitalRead(dipSwitchPin1));
-    Serial.println(digitalRead(dipSwitchPin2));
-
-    //show current temperature [placeholder =  0 && 0]
-    display(0);
-    digitalWrite(12, LOW);
-    digitalWrite(9, HIGH);
-
-    delay(10);
-    digitalWrite(12, HIGH);
-    digitalWrite(9, LOW);
-    display(0);
-    delay(10);
-  }
-  else
-  {
-    Serial.println("ERROR");
-    Serial.println(digitalRead(dipSwitchPin1));
-    Serial.println(digitalRead(dipSwitchPin2));
-    //show 1 && 0
-    display(1);
-    digitalWrite(12, LOW);
-    digitalWrite(9, HIGH);
-
-    delay(10);
-    digitalWrite(12, HIGH);
-    digitalWrite(9, LOW);
-    display(0);
-    delay(10);
-  }
-  delay(1000);
 }
